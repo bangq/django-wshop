205cbb35560eb9562352d0ea6db63e93fdf05ef9
bangq <yubq@yongjiasoft.com>
2017-07-20 20:46:29 +0800

    筛选，逻辑删除

--- apps/product/admin.py
+++ apps/product/admin.py
@@ -5,11 +5,34 @@ from .models import Goods, Category
 
 # Register your models here.
 
+
 class GoodsAdmin(admin.ModelAdmin):
     search_fields = ['name']
-    list_display = ['name', 'cost_price', 'price', 'count']
+    list_display = ['category', 'name', 'cost_price', 'price', 'count', 'status']
+    list_display_links = ('name',)
+    list_filter = ('category', 'status', 'is_abort',)
+    fields = ('category', 'name', 'keywords', 'image', 'details', ('count', 'sales_count'),
+              ('market_price', 'cost_price', 'price'),
+              ('is_show_sales_count', 'has_invoice', 'cannot_refund', 'no_search'), 'status')
 
-    fields = ('category', 'name', 'image', 'details', 'barcode', 'cost_price', 'price', 'count')
+    actions = ['make_on_sale', 'make_off_sale', 'delete_selected']
+
+    def make_on_sale(self, request, queryset):
+        rows_updated = queryset.update(status=1)
+        self.message_user(request, "上架了 %s 条商品." % rows_updated)
+
+    make_on_sale.short_description = "上架选中的商品"
+
+    def make_off_sale(self, request, queryset):
+        rows_updated = queryset.update(status=0)
+        self.message_user(request, "下架了 %s 条商品" % rows_updated)
+
+    make_off_sale.short_description = "下架选中的商品"
+
+    def delete_selected(self, request, queryset):
+        queryset.update(is_abort=True)
+
+    delete_selected.short_description = '删除所选项'
 
     class Media:
         js = (
@@ -26,7 +49,17 @@ class CategoryAdmin(admin.ModelAdmin):
     search_fields = ['name']
     list_display = ['name']
     fields = ('name', 'sort')
-    model_icon = "fa fa-bars"
+    actions = ['delete_selected']
+
+    def delete_selected(self, request, queryset):
+        queryset.update(is_abort=True)
+
+    delete_selected.short_description = '删除所选项'
+
+    #   重写queryset 自定义筛选
+    def get_queryset(self, request):
+        qs = super(CategoryAdmin, self).get_queryset(request)
+        return qs.filter(is_abort=False)
 
 
 admin.site.register(Goods, GoodsAdmin)
