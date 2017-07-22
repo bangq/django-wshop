a9d35a5e27d4418fc75287488dd1da15ed50a2fb
bangq <yubq@yongjiasoft.com>
2017-07-22 19:59:32 +0800

    新增了 支付的配置和商城的配置

---
+++ config/PaySetting.py
@@ -0,0 +1,22 @@
+"""
+ 支付的配置文件
+"""
+# 微信支付 ，默认开启
+WECHAT_PAY = True
+# 货到付款， 默认关闭
+CASH = False
+# 支付宝支付，默认关闭
+ALIPAY = False
+
+# 微信公众号 appid
+WECHAT_APPID = ''
+# 商户 key
+WECHAT_APP_KEY = ''
+# 商户号
+WECHAT_MCH_ID = ''
+# 可选，子商户号，受理模式下必填
+WECHAT_SUB_MCH_ID = ''
+# 必填，商户证书路径
+WECHAT_MCH_CERT = ''
+# 必填，商户证书私钥路径
+WECHAT_KEY = ''
---
+++ config/ShopSetting.py
@@ -0,0 +1,37 @@
+"""
+ 商城的配置文件
+
+"""
+
+# 店铺名称
+SHOP_NAME = "W-SHOP"
+
+# 商城是否关闭
+Is_Close = False
+
+# 获取未关注者信息
+Is_GetInfo = False
+
+# 预警库存数量
+STOCK_WARN = 0
+
+# 未付款订单几天后关闭
+CLOSE_ORDER = 0
+
+# 关闭前提前多久提醒用户
+WILL_CLOSE_ORDER = 30
+
+# 系统积分名称
+INTEGRAL_NAME = '积分'
+
+# 余额名称
+BALANCE_NAME = '余额'
+
+# 购物赠送积分
+IS_SHOPPING_ADD_INTEGRAL = True
+
+# 积分比例 消费1元多多少积分
+INTEGRAL_RATIO = 1
+
+# 积分上限，0为不限制
+MAX_INTEGRAL = 0
