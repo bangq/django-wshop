b6c6a5a8a460d32e58c97d668060d42545eaabb5
bangq <yubq@yongjiasoft.com>
2017-07-22 21:01:38 +0800

    ..

--- apps/shop/apps.py
+++ apps/shop/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class ShopConfig(AppConfig):
     name = 'shop'
+    verbose_name = '商城管理'
--- wshop/settings.py
+++ wshop/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'user',
     'product',
     'order',
+    'shop',
 
 ]
 
